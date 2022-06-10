f6ae3a451590dc2756e37a6718ec0fea08cb39fb
inf19025 <inf19025@lehre.dhbw-stuttgart.de>
2022-06-10 22:42:56 +0200

    mlflow traefik settings korrekt implementieren & aufräumen

--- launch_mlflow.sh
+++ launch_mlflow.sh
@@ -3,8 +3,12 @@
 # add tljh python bin to path, so mlflow can find gunicorn
 export PATH="/opt/tljh/user/bin/:$PATH"
 
+{% for var in extra_env_vars %}
+    export '{{ var.name }}'="'{{ var.value }}'"
+{% endfor %}
+
 cd /opt/tljh/user/bin
 
 ./python3 mlflow server -h 0.0.0.0 \
-                        --backend-store-uri sqlite:////mlflow/mlflow.db \
-                        --default-artifact-root {{ mlflow_artifact_storage }}
+        --backend-store-uri sqlite:////mlflow/mlflow.db \
+        --default-artifact-root {{ mlflow_artifact_storage }}
